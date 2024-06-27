5c5c8b20dae9045781e43a4b3d134655ecd2a35e
liyinxia <liyinxia@weimiao.cn>
2024-06-27 19:17:36 +0800

    feat: 增加压缩

--- config/index.js
+++ config/index.js
@@ -33,6 +33,9 @@ const config = {
     enable: false // Webpack 持久化缓存配置，建议开启。默认配置请参考：https://docs.taro.zone/docs/config-detail#cache
   },
   mini: {
+    optimizeMainPackage: {
+      enable: true
+    },
     webpackChain (chain) {
       chain.merge({
         module: {
